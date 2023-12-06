96a88bee300ffd93e3005f03dd8dbf310bcf343a
Electrolytful <erhanulusal@hotmail.com>
2023-12-06 13:43:08 +0000

    fixed bugs involving adding decimal points to parts of the calculation that made no sense

--- src/components/ButtonMatrix/ButtonMatrix.jsx
+++ src/components/ButtonMatrix/ButtonMatrix.jsx
@@ -5,13 +5,12 @@ import { useCalc } from "../../hooks/useCalc.jsx";
 
 import Button from "../Button/Button";
 
-
 const btnMatrix = [
-    ["C", "DEL", "%", "/"],
-    [7, 8, 9, "*"],
-    [4, 5, 6, "-"],
-    [1, 2, 3, "+"],
-    [0, ".", "="],
+  ["C", "DEL", "%", "/"],
+  [7, 8, 9, "*"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
 ];
 
 const ops = ["/", "*", "+", "-", ".", "%"];
@@ -19,77 +18,95 @@ const ops = ["/", "*", "+", "-", ".", "%"];
 const dangerValues = ["NaN", "Infinity"];
 
 export default function ButtonMatrix() {
-  const { calc, dispatch } = useCalc();
+  const { calc, allowDecimal, dispatch } = useCalc();
 
   useEffect(() => {
     const refreshResult = () => {
-      if(calc != "" && !ops.includes(calc.slice(-1))) {
-        dispatch({type: "UPDATE_RESULT", payload: eval(calc).toString()});
+      if (calc != "" && !ops.includes(calc.slice(-1))) {
+        dispatch({ type: "UPDATE_RESULT", payload: eval(calc).toString() });
       }
 
-      if(calc == "") {
-        dispatch({type: "UPDATE_RESULT", payload: ""});
+      if (calc == "") {
+        dispatch({ type: "UPDATE_RESULT", payload: "" });
       }
-    }
+    };
 
     const updateTitle = () => {
       document.title = `Calc.io ${calc ? "|" : ""} ${calc ? calc : ""}`;
-    }
+    };
 
     refreshResult();
     updateTitle();
   }, [calc]);
 
   const calculate = () => {
-    if(calc != "" && !dangerValues.includes(calc)) {
+    if (calc != "" && !dangerValues.includes(calc)) {
       const payload = eval(calc).toString();
-      dispatch({type: "SET_CALC", payload});
+      dispatch({ type: "SET_CALC", payload });
     }
-  }
+  };
 
   const del = () => {
-    if(calc != "" && !dangerValues.includes(calc)) {
+    if (calc != "" && !dangerValues.includes(calc)) {
       const payload = calc.slice(0, -1);
-      dispatch({type: "SET_CALC", payload});
+      dispatch({ type: "SET_CALC", payload });
+
+      calc.slice(-1).toString() === "." &&
+        dispatch({ type: "SET_ALLOW_DECIMAL_TRUE" });
     }
-  }
+  };
 
   const reset = () => {
-    if(calc != ""){
-      dispatch({type: "SET_CALC", payload: ""});
+    if (calc != "") {
+      dispatch({ type: "SET_CALC", payload: "" });
+      dispatch({ type: "SET_ALLOW_DECIMAL_TRUE" });
     }
-  }
+  };
 
   const handleClick = (value) => {
-    if(ops.includes(value) && calc === "" || ops.includes(value) && ops.includes(calc.slice(-1))) {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1))) ||
+      (value.toString() === "." && !allowDecimal)
+    ) {
       return;
     }
 
-    if(value.toString() === "DEL") {
+    if (dangerValues.includes(calc)) {
+      return;
+    }
+
+    if (value.toString() === "DEL") {
       del();
       return;
     }
 
-    if(value.toString() === "=") {
+    if (value.toString() === "=") {
       calculate();
       return;
     }
 
-    if(value.toString() === "C") {
+    if (value.toString() === "C") {
       reset();
       return;
     }
 
-    if(!dangerValues.includes(calc)) {
-      dispatch({type: "UPDATE_CALC", payload: value.toString()});
+    if (value.toString() === ".") {
+      dispatch({ type: "SET_ALLOW_DECIMAL_FALSE" });
     }
-  }
+
+    if (ops.includes(value.toString()) && value.toString() != ".") {
+      dispatch({ type: "SET_ALLOW_DECIMAL_TRUE" });
+    }
+
+    dispatch({ type: "UPDATE_CALC", payload: value.toString() });
+  };
 
   return (
     <div className={styles.btnMatrix}>
-        {btnMatrix.flat().map((btn, i) => (
-            <Button value={btn} key={i} onClick={() => handleClick(btn)} />
-        ))}
+      {btnMatrix.flat().map((btn, i) => (
+        <Button value={btn} key={i} onClick={() => handleClick(btn)} />
+      ))}
     </div>
-  )
+  );
 }
--- src/context/CalcContext.jsx
+++ src/context/CalcContext.jsx
@@ -1,49 +1,66 @@
 import { createContext, useReducer } from "react";
 
 export const CalcContext = createContext({
-    calc: "",
-    result: "",
+  calc: "",
+  result: "",
+  allowDecimal: true,
 });
 
 export const calcReducer = (state, action) => {
-    switch(action.type) {
-        case "SET_CALC":
-            return {
-                calc: action.payload,
-                result: state.result
-            }
+  switch (action.type) {
+    case "SET_CALC":
+      return {
+        calc: action.payload,
+        result: state.result,
+        allowDecimal: state.allowDecimal,
+      };
 
-        case "UPDATE_CALC":
-            return {
-                calc: state.calc + action.payload,
-                result: state.result
-            }
+    case "UPDATE_CALC":
+      return {
+        calc: state.calc + action.payload,
+        result: state.result,
+        allowDecimal: state.allowDecimal,
+      };
 
-        case "UPDATE_RESULT":
-            return {
-                calc: state.calc,
-                result: action.payload
-            }
+    case "UPDATE_RESULT":
+      return {
+        calc: state.calc,
+        result: action.payload,
+        allowDecimal: state.allowDecimal,
+      };
 
-        default:
-            return state;
-    }
-}
+    case "SET_ALLOW_DECIMAL_TRUE":
+      return {
+        calc: state.calc,
+        result: state.result,
+        allowDecimal: true,
+      };
+
+    case "SET_ALLOW_DECIMAL_FALSE":
+      return {
+        calc: state.calc,
+        result: state.result,
+        allowDecimal: false,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export const CalcContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(calcReducer, {
-        calc: "",
-        result: "",
-    });
+  const [state, dispatch] = useReducer(calcReducer, {
+    calc: "",
+    result: "",
+    allowDecimal: true,
+  });
 
-    const context = {
-        ...state,
-        dispatch
-    }
+  const context = {
+    ...state,
+    dispatch,
+  };
 
-    return (
-        <CalcContext.Provider value={context}>
-            {children}
-        </CalcContext.Provider>
-    );
-}
+  return (
+    <CalcContext.Provider value={context}>{children}</CalcContext.Provider>
+  );
+};
